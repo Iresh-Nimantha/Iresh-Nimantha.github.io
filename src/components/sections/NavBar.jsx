d32a28119ec36d57811202c95ce1e649a9916638
Iresh Nimantha <167967700+Iresh-Nimantha@users.noreply.github.com>
2025-03-31 01:27:30 +0530

    Update NavBar.jsx

--- src/components/sections/NavBar.jsx
+++ src/components/sections/NavBar.jsx
@@ -9,11 +9,11 @@ const NavBar = () => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
 
   const navItems = [
-    { name: "Home", link: "#home" },
-    { name: "About Me", link: "#about" },
-    { name: "Skills", link: "#skills" },
-    { name: "Projects", link: "#projects" },
-    { name: "Contact Me", link: "#contact" },
+   { name: "Home", link: "/#home" },
+  { name: "About Me", link: "/#about" },
+  { name: "Skills", link: "/#skills" },
+  { name: "Projects", link: "/#projects" },
+  { name: "Contact Me", link: "/#contact" },
   ];
 
   useEffect(() => {
