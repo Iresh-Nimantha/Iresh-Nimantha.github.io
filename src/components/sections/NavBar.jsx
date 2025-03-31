1dfbb8284afb811e5f619553d27f284433a482fa
Iresh Nimantha <167967700+Iresh-Nimantha@users.noreply.github.com>
2025-03-31 22:06:59 +0530

    Update NavBar.jsx

--- src/components/sections/NavBar.jsx
+++ src/components/sections/NavBar.jsx
@@ -7,11 +7,14 @@ const NavBar = () => {
   const [scrolled, setScrolled] = useState(false);
 
   const navItems = [
-    { name: "Home", link: "#home" },
-    { name: "About Me", link: "#about" },
-    { name: "Skills", link: "#skills" },
-    { name: "Projects", link: "#projects" },
-    { name: "Contact Me", link: "#contact" },
+const navItems = [
+  { name: "Home", link: "/#/all#home" },
+  { name: "About Me", link: "/#/all#about" },
+  { name: "Skills", link: "/#/all#skills" },
+  { name: "Projects", link: "/#/all#projects" },
+  { name: "Contact Me", link: "/#/all#contact" },
+];
+
   ];
 
   useEffect(() => {
