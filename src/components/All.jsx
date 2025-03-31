845ec64917baeedaf94f2659a3be197b05e683e6
Iresh Nimantha <167967700+Iresh-Nimantha@users.noreply.github.com>
2025-03-31 22:50:24 +0530

    Update All.jsx

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -1,34 +1,10 @@
-// All.jsx (Simplified for debugging)
-import React, { useEffect, useRef } from "react";
-import NavBar from "./sections/NavBar";
-// import HeroSection from "./sections/HeroSection";
-// import AboutMeSection from "./sections/AboutSection";
-// import SkillsSection from "./sections/SkillsSection";
-// import ProjectsSection from "./sections/ProjectsSection";
-// import Contact from "./sections/Contact";
-// import FooterSection from "./sections/FooterSection";
+// All.jsx (Minimal Version)
+import React from 'react';
 
 function All() {
-  useEffect(() => {
-    const hash = window.location.hash;
-    if (hash) {
-      const element = document.querySelector(hash);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, []);
-
   return (
     <div>
-      <NavBar />
-      {/* <HeroSection /> */}
-      {/* <AboutMeSection /> */}
-      {/* <SkillsSection /> */}
-      {/* <ProjectsSection /> */}
-      {/* <Contact /> */}
-      {/* <FooterSection /> */}
-      <h1>All Component Loaded</h1>
+      <h1>All Component Loaded (Testing)</h1>
     </div>
   );
 }
