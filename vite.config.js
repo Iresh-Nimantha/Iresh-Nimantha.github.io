6bd748e36bb5ed567a0864d411c34e2d7c2bdc82
Iresh Nimantha <167967700+Iresh-Nimantha@users.noreply.github.com>
2025-07-03 23:00:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [stringPlugin(), react()],
   assetsInclude: ["**/*.glb"],
-  base: "/Iresh-Nimantha.github.io/",
+  base: "/iresh-nimantha.github.io/",
 });
